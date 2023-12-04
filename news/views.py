c7501f13fce5fcb8d66fdf4ce1c245e75b9ed0f2
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-12-04 18:38:41 -0300

    task 04 - create home

--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+
+def home(request):
+    news_list = News.objects.all()
+    return render(request, 'home.html', {'news_list': news_list})
